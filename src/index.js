53d55ddfe72c47d0316cc4e58c74d95ae5f8a281
GaraninMS <maxgaranin@gmail.com>
2019-10-02 23:47:07 +0400

    feat: first draft

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let n = +first;
+
+  let ks = [];
+  for (let i = second.length - 1; i >= 0; i--) {
+    ks.push(+second[i]);
+  }
+
+  let result = 0;
+  let i = 0;
+  for (const k of ks) {
+    let mult = n * (k * Math.pow(10, i));
+    result += mult;
+    i++;
+  }
+
+  return result;
 }
