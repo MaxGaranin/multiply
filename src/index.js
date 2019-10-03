e0d22ccbc5235ca968f59a15993fe4d870e786fe
GaraninMS <GaraninMS@samnipineft.ru>
2019-10-03 10:22:18 +0400

    feat: add multiply realization by manual column technic

--- src/index.js
+++ src/index.js
@@ -1,18 +1,51 @@
 module.exports = function multiply(first, second) {
-  let n = +first;
-
-  let ks = [];
-  for (let i = second.length - 1; i >= 0; i--) {
-    ks.push(+second[i]);
+  if (second.length > first.length) {
+    [first, second] = [second, first];
   }
 
-  let result = 0;
-  let i = 0;
-  for (const k of ks) {
-    let mult = n * (k * Math.pow(10, i));
-    result += mult;
-    i++;
+  let a = strToReverseArray(first);
+  let b = strToReverseArray(second);
+  
+  let res = [];
+  for (let i = 0; i < a.length + b.length; i++) {
+    res[i] = 0;
   }
 
-  return result;
+  let k = 0;
+
+  for (let i = 0; i < b.length; i++) {
+    k = i;
+    let c_decimal = 0;
+
+    for (let j = 0; j < a.length; j++) {
+      let c = a[j] * b[i] + c_decimal;
+
+      let c_reminder = c % 10;
+      c_decimal = Math.trunc(c / 10);
+
+      let acc_res = res[k] + c_reminder;
+      res[k] = acc_res % 10;
+      c_decimal += Math.trunc(acc_res / 10);
+
+      k++;
+    }
+
+    if (c_decimal > 0) {
+      res[k] += c_decimal;
+      k++;
+    }
+  }
+
+  let str_res = res.slice(0, k).reverse().join('');
+  return str_res;
+}
+
+function strToReverseArray(s) {
+  let arr = [];
+  let i = s.length - 1;
+  while (i >= 0) {
+    arr.push(s[i]);
+    i--;
+  }
+  return arr;
 }
